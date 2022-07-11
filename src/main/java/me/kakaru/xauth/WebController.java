765a842bd20b4281559ffb628f307b33cc7956c5
kakaru1331 <kakaru1331@gmail.com>
2022-07-11 17:27:44 +0900

    create a controller for request authentication

---
+++ src/main/java/me/kakaru/xauth/WebController.java
@@ -0,0 +1,40 @@
+package me.kakaru.xauth;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class WebController {
+
+    String AUTH_HEADER = "x-ext-authz";
+    String ALLOWED_VALUE = "allow";
+    String DENY_BODY = String.format("denied by ext_authz for not found header %s: %s", AUTH_HEADER, ALLOWED_VALUE);
+
+
+    @RequestMapping(value = "**", method = { RequestMethod.GET, RequestMethod.POST})
+    public ResponseEntity index(HttpServletRequest request) {
+        String authHeader = request.getHeader(AUTH_HEADER);
+
+        if (checkAuthHeader(authHeader)) {
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(DENY_BODY);
+        }
+    }
+
+    boolean checkAuthHeader(String authHeader) {
+        if (StringUtils.isEmpty(authHeader)) {
+            return false;
+        } else if (AUTH_HEADER.equals(authHeader)) {
+            return false;
+        }
+
+        return true;
+    }
+}

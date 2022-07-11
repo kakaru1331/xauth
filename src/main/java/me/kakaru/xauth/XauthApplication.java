5e4e82eb295cb034629735069f771a261c70da1e
kakaru1331 <kakaru1331@gmail.com>
2022-07-11 17:27:06 +0900

    init

---
+++ src/main/java/me/kakaru/xauth/XauthApplication.java
@@ -0,0 +1,13 @@
+package me.kakaru.xauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/kakaru/xauth/XauthApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kakaru.xauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
